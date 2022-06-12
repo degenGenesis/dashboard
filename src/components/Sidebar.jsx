17fd6f64b359361ce038619e703f02f59b26ee98
degenGenesis <degengenesis@protonmail.com>
2022-06-11 23:16:11 -0700

    Fix links div className in Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
       {/* menu links */}
       <div className='mt-10'>
         {links.map((item) => (
-          <div className='text-gray-400 m-3 mt-4 uppercase'>
+          <div key={item.title}>
             {item.title}
           </div>
         ))}
