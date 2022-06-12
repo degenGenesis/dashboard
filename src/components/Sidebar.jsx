c5d1f19107f00af565f8a5bf9722a98488c140bd
degenGenesis <degengenesis@protonmail.com>
2022-06-11 23:19:41 -0700

    Add styling to Sidebar nav links in Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,7 +31,9 @@ const Sidebar = () => {
       <div className='mt-10'>
         {links.map((item) => (
           <div key={item.title}>
-            {item.title}
+            <p className='text-gray-400 m-3 mt-4 uppercase'>
+              {item.title}
+            </p>
           </div>
         ))}
       </div>
