567e8f96490102aaa338a26579f07778ac3865d0
degenGenesis <degengenesis@protonmail.com>
2022-06-12 22:21:19 -0700

    added comments to Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,16 +7,21 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { links } from '../data/dummy';
 
 const Sidebar = () => {
+  // displays sidebar
   const activeMenu = true;
 
+  // defines & styles active & inactive nav links 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   return (
+    // Sidebar div
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       
       {/* Sidebar active */}
       {activeMenu && (<>
+      
+      {/* hero icon div */}
       <div className='flex justify-between items-center'>
         <Link to='/' onClick={() => {}} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
           <SiShopware /> <span>Shoppy</span>
@@ -30,13 +35,17 @@ const Sidebar = () => {
         </TooltipComponent>
       </div>
 
-      {/* menu links */}
+      {/* Sidebar menu sections & nav links */}
       <div className='mt-10'>
+        
+        {/* sections */}
         {links.map((item) => (
           <div key={item.title}>
             <p className='text-gray-400 m-3 mt-4 uppercase'>
               {item.title}
             </p>
+
+            {/* dynamically sets active & inactive nav links */}
             {item.links.map((link) => (
               <NavLink
                 to={`/${link.name}`}
@@ -45,7 +54,10 @@ const Sidebar = () => {
                 className={({ isActive }) => 
               isActive ? activeLink : normalLink}
               >
+                {/* link icons */}
                 {link.icon}
+
+                {/* nav link text */}
                 <span className='capitalize'>
                   {link.name}
                 </span>
