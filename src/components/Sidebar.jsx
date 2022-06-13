d4f227775a7666bd55f1e19fd1e67dd2bd7f7054
degenGenesis <degengenesis@protonmail.com>
2022-06-12 23:38:52 -0700

    Implement onClick props in Sidebar.js for disabling/enabling inactive/active Sidebar menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -26,13 +26,19 @@ const Sidebar = () => {
       
       {/* hero icon div */}
       <div className='flex justify-between items-center'>
-        <Link to='/' onClick={() => {}} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+        <Link 
+          to='/' 
+          onClick={() => setActiveMenu(false)} 
+          className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
           <SiShopware /> <span>Shoppy</span>
         </Link>
 
         {/* Collapse Sidebar button */}
         <TooltipComponent content='Menu' position='BottomCenter'>
-          <button type='button' onClick={() => {}} className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
+          <button 
+            type='button' 
+            onClick={() => setActiveMenu(() => !activeMenu)} 
+            className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
             <MdOutlineCancel />
           </button>
         </TooltipComponent>
