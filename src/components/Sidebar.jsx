aafc26cd084bbed23384503e33c91c75e20fc752
degenGenesis <degengenesis@protonmail.com>
2022-06-12 23:42:17 -0700

    Modified onClick property of Sidebar collapse/hide menu button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
         <TooltipComponent content='Menu' position='BottomCenter'>
           <button 
             type='button' 
-            onClick={() => setActiveMenu(() => !activeMenu)} 
+            onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} 
             className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
             <MdOutlineCancel />
           </button>
