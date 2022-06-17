11afa20a3476113fa137079dced9e9d882b257d2
degenGenesis <degengenesis@protonmail.com>
2022-06-17 15:07:42 -0700

    Import necessary packages  and components into Ecommerce.jsx

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { BsCurrencyDollary } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+
+import { Stacked, Pie, Button, Sparkline } from '../components';
+import { earningData, SparklineAreaData, ecomPieChartDat } from '../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Ecommerce = () => {
   return (
     <div>Ecommerce</div>
   )
-}
+};
 
-export default Ecommerce
+export default Ecommerce;
