d29ab4dd509628db6a062121d483fe450c6a0dd0
degenGenesis <degengenesis@protonmail.com>
2022-07-06 23:41:00 -0700

    Add imports to begin building <Sparkline /> component in components/Charts/Sparkline.jsx

--- src/components/Charts/Sparkline.jsx
+++ src/components/Charts/Sparkline.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+
 
 const Sparkline = () => {
   return (
-    <div>Sparkline</div>
+    
+    <div>
+
+    </div>
   )
 }
 
