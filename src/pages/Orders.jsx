308a18e48ba6adce80f80cc65d6a46463fadcb69
degenGenesis <degengenesis@protonmail.com>
2022-07-12 22:15:21 -0700

    Add <Header /> import & component in Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 import { orderData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div>
+      <Header category='Page' title='Orders' />
+    </div>
   )
 }
 
