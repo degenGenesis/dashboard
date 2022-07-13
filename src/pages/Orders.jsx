322f0f1917605541a1d1c1e5dcbb908078cdafbf
degenGenesis <degengenesis@protonmail.com>
2022-07-12 22:12:08 -0700

    Add imports in Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import { orderData, contextMenuItems, ordersGrid } from '../data/dummy';
 
 const Orders = () => {
   return (
