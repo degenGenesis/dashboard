2cbce33ab4908e9d78fe3fc4f6034d50a68cb824
degenGenesis <degengenesis@protonmail.com>
2022-07-15 18:48:34 -0700

    Add temp data source to Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
-import { orderData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Orders = () => {
   return (
     <div className='m-10 md:m-10 p02 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title='Orders' />
-      <GridComponent>
+      <GridComponent
+        id='gridcomp'
+        dataSource={ordersData}
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (<ColumnDirective key={index} {...item} /> 
         ))}
