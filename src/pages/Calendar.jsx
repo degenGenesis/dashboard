db842bbc0ff2ee2126294dc34d82f337e7b3fc7f
degenGenesis <degengenesis@protonmail.com>
2022-07-20 20:10:27 -0700

    Begin building out <Calendar /> component in Calendar.jsx

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Calendar = () => {
   return (
     <div>Calendar</div>
   )
-}
+};
 
-export default Calendar
+export default Calendar;
