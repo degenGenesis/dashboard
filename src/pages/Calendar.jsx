75d49c15610470ab40bd26013422c1378f88c6fd
degenGenesis <degengenesis@protonmail.com>
2022-07-22 20:34:50 -0700

    Add state management to Scheduler in Calendar.jsx

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
 import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
 
 import { scheduleData } from '../data/dummy';
 import { Header } from '../components';
 
-const Calendar = () => {
+const PropertyPane = (props) => <div className='mt-5'>{props.children}</div>;
+
+const Scheduler = () => {
+  const [scheduleObj, setScheduleObj] = useState();
+
+  const change = (args) => {
+    scheduleObj.selectedDate = args.value;
+    scheduleObj.dataBind();
+  };
+  
   return (
     <div>Calendar</div>
   )
 };
 
-export default Calendar;
+export default Scheduler;
