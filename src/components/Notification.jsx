5179be231a92866362d345254620f239ff8eda07
degenGenesis <degengenesis@protonmail.com>
2022-08-26 09:58:19 -0700

    Add: notifcations popup and test data

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -9,6 +9,7 @@ const Notification = () => {
   const { currentColor } = useStateContext();
 
   return (
+    
     <div className='nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96'>
       <div className='flex justify-between items-center'>
         <div className='flex gap-3'>
@@ -16,6 +17,17 @@ const Notification = () => {
           <button type='button' className='text-white text-xs rounded p-1 px-2 bg-orange-theme'>5 new</button>
         </div>
         
+        <div className='mt-5'>
+          {chatData?.map((item, index) => (
+            <div key={index} className='flex items-center leading-8 gap-5 border-b-1 borderColor p-3'>
+              <img className='rounded-full h-10 w-10' src={item.image} alt={item.message} />
+              <div>
+                <p className='font-semibold dark:text-gray-200'>{item.message} </p>
+                <p className='text-gray-500 text-sm dark:text-gray-400'>{item.desc} </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
