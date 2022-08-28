25aceb3bd4475881dd232324cec0d0e4ab70af73
degenGenesis <degengenesis@protonmail.com>
2022-08-27 21:49:43 -0700

    Add: style property. | Revert: text color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ import { useStateContext } from '../contexts/ContextProvider';
 const Sidebar = () => {
   
   // state variables
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, screenSize } = useStateContext();
 
   const handleCloseSidebar = () => {
     if (activeMenu !== undefined && screenSize <= 900) {
@@ -19,7 +19,7 @@ const Sidebar = () => {
   };
 
   // global variables 
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-red-600 text-md m-2';
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   return (
@@ -67,6 +67,9 @@ const Sidebar = () => {
                 to={`/${link.name}`}
                 key={link.name}
                 onClick={handleCloseSidebar}
+                style={({ isActive}) => ({
+                  backgroundColor: isActive ? currentColor : '',
+                })}
                 className={({ isActive }) => 
               isActive ? activeLink : normalLink}
               >
